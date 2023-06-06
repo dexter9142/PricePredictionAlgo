dd8f5d9136d598c4a08246a844f23a62faebbe16
Daniel <dax0068@gmail.com>
2023-06-07 01:08:41 +0300

    rounded prices

--- server.py
+++ server.py
@@ -56,26 +56,13 @@ def forecast_prices():
     xgb_input = np.append(y[-2:], np.array(next_prices_sarima[:-2]).reshape(-1, 1), axis=0)
     next_prices_xgb = xgb_model.predict(xgb_input).tolist()
 
-    # Append the forecasted prices to the array of prices
-    future_prices = np.append(prices, next_prices_sarima)
-    date_strings = [date.strftime("%d-%m-%y") for date in dates]
-
-    # Plot the actual prices, SARIMA predictions, and XGBoost predictions
-    plt.figure(figsize=(10, 6))
-    plt.plot(date_strings, prices, marker='o', label='Actual Prices')
-    plt.plot(future_dates, future_prices[-13:], marker='o', label='SARIMA Predictions')
-    plt.plot(future_dates, next_prices_xgb[-13:], marker='o', label='XGBoost Predictions')
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('Price Forecast')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # Append the forecasted prices to the array of prices starting from the next day
+    forecasted_prices = np.append(prices, next_prices_sarima[1:])
 
     # Create a list of dictionaries containing the dates and forecasted prices
     data_list = []
-    for date, price in zip(future_dates, next_prices_sarima):
-        formatted_date = datetime.strptime(date, "%d-%m-%y").strftime("%Y-%m-%d")
+    for date, price in zip(dates, forecasted_prices):
+        formatted_date = date.strftime("%Y-%m-%d")
         data_list.append({
             "date": formatted_date,
             "price": price
@@ -88,6 +75,7 @@ def get_data():
     json_data = json.dumps(forecast_prices())
     return json_data
 
+
 @app.route('/prices', methods=['POST'])
 def process_prices():
     # Get the JSON data from the request
@@ -95,40 +83,60 @@ def process_prices():
     global dates, prices
 
     # Extract the dates and prices from the payload
-    dates = [item['date'] for item in payload]
-    prices = [item['price'] for item in payload]
+    user_dates = [item['date'] for item in payload]
+    user_prices = [item['price'] for item in payload]
 
     # Convert dates to datetime objects
-    dates = [datetime.strptime(date, "%Y-%m-%d") for date in dates]
+    user_dates = [datetime.strptime(date, "%Y-%m-%d") for date in user_dates]
 
     # Perform any necessary data preprocessing or modeling steps here
     # ...
 
-    # Create SARIMA model
-    y = np.array(prices).reshape(-1, 1)
+    # Create SARIMA model using user-provided prices
+    y = np.array(user_prices).reshape(-1, 1)
     sarima_model = SARIMAX(y.flatten(), order=(1, 1, 1), seasonal_order=(1, 1, 1, 7))
     sarima_model_fit = sarima_model.fit()
 
     # Forecast prices for the provided dates using SARIMA
-    forecasted_prices = sarima_model_fit.forecast(steps=len(dates)).tolist()
+    forecasted_prices = sarima_model_fit.forecast(steps=len(user_dates)).tolist()
 
     # Format the forecasted prices to have one digit after the decimal point
     forecasted_prices = [round(price, 1) for price in forecasted_prices]
 
-    # Create a list of dictionaries containing the dates and formatted forecasted prices
+    # Create a list of dictionaries containing the dates and forecasted prices
     response = []
-    for date, price in zip(dates, forecasted_prices):
+
+    # Append the user-provided prices to the response
+    for date, price in zip(user_dates, user_prices):
         formatted_date = date.strftime("%Y-%m-%d")
         response.append({
             "date": formatted_date,
             "price": price
         })
 
+    # Get the last date from the user-provided dates
+    last_date = user_dates[-1]
+
+    # Generate the next day's date
+    next_date = last_date + timedelta(days=1)
+
+    # Append the forecasted prices to the response starting from the next day
+    for price in forecasted_prices:
+        formatted_date = next_date.strftime("%Y-%m-%d")
+        response.append({
+            "date": formatted_date,
+            "price": price
+        })
+        next_date += timedelta(days=1)
+
     # Convert the response to JSON
     json_data = json.dumps(response)
 
     # Return the JSON data
     return json_data
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
